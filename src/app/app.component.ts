12aae48be60aee48fc27965a0da7b19e5d304491
mrChernicharo <felipe.chernicharo@gmail.com>
2020-07-29 10:46:18 -0300

    Pipes -> transforming the way data is dislayed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,25 +11,25 @@ export class AppComponent {
       instanceType: 'medium',
       name: 'Production Server',
       status: 'stable',
-      started: new Date(15, 1, 2017)
+      started: new Date(2020, 7, 29)
     },
     {
       instanceType: 'large',
       name: 'User Database',
       status: 'stable',
-      started: new Date(15, 1, 2017)
+      started: new Date(2020, 7, 29)
     },
     {
       instanceType: 'small',
       name: 'Development Server',
       status: 'offline',
-      started: new Date(15, 1, 2017)
+      started: new Date(2020, 7, 29)
     },
     {
       instanceType: 'small',
       name: 'Testing Environment Server',
       status: 'stable',
-      started: new Date(15, 1, 2017)
+      started: new Date(2020, 7, 29)
     }
   ];
   getStatusClasses(server: {instanceType: string, name: string, status: string, started: Date}) {
